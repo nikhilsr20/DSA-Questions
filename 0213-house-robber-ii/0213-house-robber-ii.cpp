417ff5ae30390575c64873f946c6fe0e18ee3885
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-03-03 16:08:35 +0530

    Time: 0 ms (100%), Space: 10.7 MB (53.06%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -32,6 +32,9 @@ public:
         if(nums.size()==1){
             return nums[0];
         }
+        if(nums.size()==2){
+            return max(nums[0],nums[1]);
+        }
         vector<int> dp(nums.size(),-1);
         vector<int> dp2(nums.size(),-1);
 
