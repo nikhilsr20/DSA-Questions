b6443a27ffac0cea8a2fd82faec653fb8c4fd6cb
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-03-04 15:21:37 +0530

    Time: 102 ms (69.41%), Space: 111.3 MB (15.34%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,42 +1,19 @@
-// int find(int &need,int curr,vector<int> &nums,int i,vector<vector<int>> &dp){
-//     if(curr>need){
-//         return 0;
-//     }
-//     if(i==nums.size()){
-//         if(curr==need){
-//             return 1;
-//         }
-//         return 0;
-//     }
 
-//     if(dp[i][curr]!=-1){
-//         return dp[i][curr];
-//     }
-//     int x=find(need,curr+nums[i],nums,i+1,dp);
-//     int y=find(need,curr,nums,i+1,dp);
-    
-//     return dp[i][curr]=x|y;
-
-// }
-
-int find(int &need,vector<int> &nums, vector<int> &dp){
-    dp[0]=1;
+int find(int &need,vector<int> &nums, vector<vector<int>> &dp){
+    dp[0][0]=1;
 
     for(int i=0;i<nums.size();i++){
-        // if(nums[i]<=need){
-        // dp[nums[i]]=1;
-        // }
-        // else{
-        //     return 0;
-        // }
+       
      for(int j=1;j<=need;j++){
 
-        if(j-nums[i]>=0){
-        dp[j]=dp[j-nums[i]]|dp[j];
-        }
+       dp[i+1][j] = dp[i][j];  // not take
+
+    if(j-nums[i] >= 0){
+        dp[i+1][j] = dp[i+1][j] || dp[i][j-nums[i]]; // take
     }
     }
-    return dp[need];
+    }
+    return dp[nums.size()][need];
 
     
 
@@ -54,8 +31,11 @@ public:
         return false;
     }
     // bool ans=false;
-    vector<int> dp(need+1,0);
+      vector<vector<int>> dp(nums.size()+1,vector<int> (need+1,0));
     int p=find(need,nums,dp);
+
+   
+
     return p;
 
 
