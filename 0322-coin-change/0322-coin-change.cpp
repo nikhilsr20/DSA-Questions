add706f1195fd3da50097483a746c0b78f418b04
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-03-04 16:20:06 +0530

    Time: 47 ms (34.03%), Space: 53 MB (12.7%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,54 @@
-int find(int amount,vector<int> &coins,int i,long long currsum,vector<vector<int>> &dp){
-    if(currsum>amount) return INT_MAX;
-    if(i==coins.size()){
-         if(currsum==amount){
+// int find(int amount,vector<int> &coins,int i,long long currsum,vector<vector<int>> &dp){
+//     if(currsum>amount) return INT_MAX;
+//     if(i==coins.size()){
+//          if(currsum==amount){
            
-            return 0;
-         }
-         return INT_MAX;
+//             return 0;
+//          }
+//          return INT_MAX;
+//     }
+
+//     if(dp[i][currsum]!=-1){
+//         return dp[i][currsum];
+//     }
+
+//     int x=find(amount,coins,i+1,currsum,dp);
+//     int y=find(amount,coins,i,currsum+coins[i],dp);
+//     if(y!=INT_MAX){
+//         y++;
+//     }
+//     return dp[i][currsum]=min(x,y);
+// }
+
+int find(int amount,vector<int> &coins,int i,long long currsum,vector<vector<int>> &dp){
+    for(int i = 0; i <= coins.size(); i++){
+    dp[i][0] = 0;
+}
+    
+
+    for(int i=1;i<=coins.size();i++){
+        for(int j=1;j<=amount;j++){
+            // skip 
+            int x=dp[i-1][j];
+
+            // take
+            int y=INT_MAX;
+            if(j-coins[i-1]>=0){
+
+            
+            y=(dp[i][j-coins[i-1]]);
+            if(y!=INT_MAX){
+                y++;
+            }
+            }
+
+            dp[i][j]=min(x,y);
+            // cout<<"dp["<<i<<"]"<<"["<<j<<"]="<<dp[i][j]<<endl;
+
+        }
     }
 
-    if(dp[i][currsum]!=-1){
-        return dp[i][currsum];
-    }
-
-    int x=find(amount,coins,i+1,currsum,dp);
-    int y=find(amount,coins,i,currsum+coins[i],dp);
-    if(y!=INT_MAX){
-        y++;
-    }
-    return dp[i][currsum]=min(x,y);
+        return dp[coins.size()][amount];
 }
 
 
@@ -27,7 +58,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
     //  memoization
     int ans=INT_MAX;
-    vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
+    vector<vector<int>> dp(coins.size()+1,vector<int> (amount+1,INT_MAX));
      ans=find(amount,coins,0,0,dp);
      return (ans==INT_MAX)?-1:ans;
     }
