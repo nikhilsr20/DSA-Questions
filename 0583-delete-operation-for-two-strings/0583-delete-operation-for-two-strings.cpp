3aebbd475693ba090bfbeae37ea3eb6acbfe4b8b
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-03-05 17:45:24 +0530

    Time: 19 ms (16.81%), Space: 16.7 MB (56.65%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,33 +1,61 @@
-int find(string &word1,string &word2,vector<vector<int>> &dp,int i,int j){
-    if(i==word1.size() || j==word2.size()){
- 
-        return 0;
+// int find(string &word1,string &word2,vector<vector<int>> &dp,int i,int j){
+//     if(i==word1.size() || j==word2.size()){
+//         return 0;
+//     }
+
+//     if(dp[i][j]!=-1){
+//         return dp[i][j];
+//     }
+    
+//     int x=find(word1,word2,dp,i+1,j);
+//     int y=find(word1,word2,dp,i,j+1);
+//     int z=0;
+//     if(word1[i]==word2[j]){
+//     z=find(word1,word2,dp,i+1,j+1)+1;
+//     }
+//     return dp[i][j]=max({x,y,z});
+    
+// }
+
+ int find(string &word1,string &word2,vector<vector<int>> &dp){
+
+    for(int i=1;i<=word1.size();i++){
+        for(int j=1;j<=word2.size();j++){
+            int x=dp[i-1][j];
+            int y=dp[i][j-1];
+
+            int z=0;
+            if(word1[i-1]==word2[j-1]){
+                z=dp[i-1][j-1]+1;
+            }
+            dp[i][j]=max({x,y,z});
+        }
     }
 
-    if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    
-    int x=find(word1,word2,dp,i+1,j);
-    int y=find(word1,word2,dp,i,j+1);
-    int z=0;
-    if(word1[i]==word2[j]){
-    z=find(word1,word2,dp,i+1,j+1)+1;
-    }
-    return dp[i][j]=max({x,y,z});
-    
-}
+    return dp[word1.size()][word2.size()];
+
+
+
+
+
+
+
+
+
+ }
+
+
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        // memoizatiom
-        vector<vector<int>> dp(word1.size(),vector<int> (word2.size(),-1));
+        // tabulation
+        vector<vector<int>> dp(word1.size()+1,vector<int> (word2.size()+1,0));
         int i=0;
         int j=0;
         int n=word1.size();
         int m=word2.size();
 
-        return (n+m)-(2*find(word1,word2,dp,i,j));
+        return (n+m)-(2*find(word1,word2,dp));
 
     }
 };
