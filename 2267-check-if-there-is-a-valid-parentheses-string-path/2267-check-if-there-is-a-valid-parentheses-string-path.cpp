2247736554b41db4fbdb258b3bcf6bf746ca4c81
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-03-07 15:48:41 +0530

    Time: 906 ms (5.08%), Space: 646.5 MB (5.07%) - LeetHub

---
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -0,0 +1,50 @@
+bool find(vector<vector<char>> &grid,int i,int j,int count,vector<vector<vector<int>>> &dp){
+    if(count>5000){
+        return false;
+    }
+   
+    if(i==grid.size()-1 && j==grid[0].size()-1){
+        if(count==0){
+            return true;
+        }
+        return false;
+    }
+
+    if(dp[i][j][count]!=-1){
+        return dp[i][j][count];
+    }
+    
+     int x=false,y=false;
+    if(i+1<grid.size()){
+        if(grid[i+1][j]==')' && count>0){
+        x=find(grid,i+1,j,count-1,dp);
+        }
+        else if (grid[i+1][j]=='(' ){
+            x=find(grid,i+1,j,count+1,dp);
+        }
+    }
+    if(j+1<grid[0].size()){
+        if(grid[i][j+1]==')' && count>0){
+        y=find(grid,i,j+1,count-1,dp);
+        }
+        else if (grid[i][j+1]=='(' ){
+            y=find(grid,i,j+1,count+1,dp);
+        }
+    }
+
+    return dp[i][j][count]=x|y;
+
+}
+
+
+class Solution {
+public:
+    bool hasValidPath(vector<vector<char>>& grid) {
+        if(grid[0][0]==')') return false;
+        int x=5001;
+        vector<vector<vector<int>>> dp(grid.size(),vector<vector<int>> (grid[0].size(),vector<int> (x,-1)));
+      int count=1;
+        return find(grid,0,0,count,dp);
+     
+    }
+};
