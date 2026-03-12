ab118351175ebc7f206815906704acbed28397c5
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-03-12 16:56:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,28 @@
+
+
 int find(vector<int> &prices,int i,int stock,vector<vector<int>> &dp){
-    if(i==prices.size()){
-        return 0;
+  
+for(int i=1;i<=prices.size();i++){
+    for(int j=1;j<4;j++){
+        int x=0;
+        if(j==1){
+          x=dp[i-1][j-1]-prices[i-1];
+        }
+        else if(j==2){
+          x=dp[i-1][j-1]+prices[i-1];
+        }
+        else if(j==3){
+          x=dp[i-1][j-1];
+        }
+      
+        dp[i][j]=max(x, dp[i-1][j]);
     }
+}
+return dp[prices.size()][3];
 
-    if(dp[i][stock]!=-1){
-        return dp[i][stock];
-    }
 
-    int x=0,y=0;
 
-    if(stock==0){
-        x=find(prices,i+1,1,dp)-prices[i];
-    }
-    else if(stock==1){
-         x=find(prices,i+1,2,dp)+prices[i];
-    }
-    else{
-         x=find(prices,i+1,0,dp);
-    }
-     y=find(prices,i+1,stock,dp);
 
-     return dp[i][stock]=max(x,y);
 
 }
 
@@ -29,8 +31,8 @@ int find(vector<int> &prices,int i,int stock,vector<vector<int>> &dp){
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        //memoization
-        vector<vector<int>> dp(prices.size(),vector<int> (3,-1));
+        //tabulation
+        vector<vector<int>> dp(prices.size()+1,vector<int> (4,0));
         int stock=0;
         return find(prices,0,stock,dp);
     
