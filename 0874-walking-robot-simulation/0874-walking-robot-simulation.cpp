ba339d3883a31c4a5af127b8cc4d60beb741d6e2
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-04-06 22:51:20 +0530

    Time: 23 ms (66.4%), Space: 37.9 MB (49.07%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,65 +1,47 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        int x=0;
-        int y=0;
-        int dir=1;
-        int m=0;
-        unordered_set<long long> a;
-        for(int i=0;i<obstacles.size();i++){
-               long long key=(long long)obstacles[i][0]<<32 | (obstacles[i][1]& 0xffffffff);
-               a.insert(key);
+        // Store obstacles for quick lookup
+        set<pair<int,int>> blocked;
+        for (auto &o : obstacles) {
+            blocked.insert({o[0], o[1]});
         }
-        
-        for(int i=0;i<commands.size();i++){
-            cout<<dir<<endl;
-            if(commands[i]==-1){
-                 dir = (dir + 1) % 4;
-    
-            }
-            else if(commands[i]==-2){
-                 dir = (dir + 3) % 4;
 
-            }
-            
-            else{
-                for(int j=0;j<commands[i];j++){
-                       if(dir==1){
-                        long long key=(long long)x<<32 | ((y+1) & 0xffffffff);
-                        if(a.count(key)){
-                            break;
-                        }
-                        y++;
-               
-                       }
-                        if(dir==2){
-                             long long key=(long long)(x+1)<<32 | (y & 0xffffffff);
-                             if(a.count(key)){
-                            break;
-                        }
-                        x++;
-                       }
-                        if(dir==3){
-                             long long key=(long long)x<<32 | ((y-1) & 0xffffffff);
-                             if(a.count(key)){
-                            break;
-                        }
-                        y--;
-                       }
-                        if(dir==0){
-                            long long key=(long long)(x-1)<<32 | ((y) & 0xffffffff);
-                            if(a.count(key)){
-                            break;
-                        }
-                        x--;
-                       }  
+        // Directions: North, East, South, West
+        vector<pair<int,int>> directions = {
+            {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+        };
+
+        int x = 0, y = 0;
+        int dir = 0; // start facing North
+        int maxDist = 0;
+
+        for (int cmd : commands) {
+            if (cmd == -1) {
+                // turn right
+                dir = (dir + 1) % 4;
+            } 
+            else if (cmd == -2) {
+                // turn left
+                dir = (dir + 3) % 4;
+            } 
+            else {
+                // move forward step by step
+                while (cmd--) {
+                    int nx = x + directions[dir].first;
+                    int ny = y + directions[dir].second;
+
+                    // stop if obstacle is ahead
+                    if (blocked.count({nx, ny})) break;
+
+                    x = nx;
+                    y = ny;
+
+                    maxDist = max(maxDist, x * x + y * y);
                 }
             }
-            m=max(m,(x*x)+(y*y));
-                
         }
 
-        return m;
-        
+        return maxDist;
     }
 };
