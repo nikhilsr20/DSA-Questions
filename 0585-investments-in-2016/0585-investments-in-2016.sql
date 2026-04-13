d38f09f5b2edcf57e39a4c9b631be88cb1c94684
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-04-14 01:36:00 +0530

    Time: 616 ms (22.4%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+WITH temp AS (
+    SELECT *
+    FROM Insurance
+    WHERE (lat, lon) IN (
+        SELECT lat, lon
+        FROM Insurance
+        GROUP BY lat, lon
+        HAVING COUNT(*) = 1
+    )
+)
+
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM temp
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+);
