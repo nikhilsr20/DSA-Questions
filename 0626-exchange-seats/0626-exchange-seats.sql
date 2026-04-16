e2c132f2689e7fec614f2110303c859e6819f225
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-04-17 01:44:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
 
-select s1.id,Case
-When id%2=1 And  s1.id != (SELECT MAX(id) FROM Seat) Then (select s.student from Seat s where s1.id+1=s.id)
-When id%2=1 And  s1.id = (SELECT MAX(id) FROM Seat) Then (select s.student from Seat s where s1.id=s.id)
-Else (select s.student from Seat s where s1.id-1=s.id)
-END as student From Seat s1;
+select id,
+case 
+when id%2=0 Then lag(student,1) over (order by id)
+Else lead(student,1,student) over (order by id)
+End
+as student 
+from Seat;
