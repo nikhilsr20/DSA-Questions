4f4a4548ace0db8ef87e70bec5449951a21029e6
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-05-04 03:35:07 +0530

    Time: 211 ms (21.96%), Space: 0B (100%) - LeetHub

---
+++ 3808-find-emotionally-consistent-users/3808-find-emotionally-consistent-users.sql
@@ -0,0 +1,13 @@
+WITH temp AS (
+    SELECT user_id, COUNT(*) AS total
+    FROM reactions
+    GROUP BY user_id
+),
+temp2 AS (
+    SELECT user_id, reaction, COUNT(*) AS total
+    FROM reactions
+    GROUP BY user_id, reaction
+)
+
+select a.user_id,reaction as dominant_reaction, Round((b.total::numeric/a.total),2) as reaction_ratio from temp a left join temp2 b On a.user_id=b.user_id where a.total>=5 and (b.total::numeric/a.total)>=0.6 order by reaction_ratio Desc, user_id asc;
+-- Write your PostgreSQL query statement below
