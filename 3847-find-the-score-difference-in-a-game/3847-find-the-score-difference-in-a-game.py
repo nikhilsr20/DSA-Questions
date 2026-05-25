39abc6c05f23be1fa62eff433db30d1a6a06899c
Nikhil Singh Rathore <nikhils1620@gmail.com>
2026-05-26 00:29:24 +0530

    Time: 11 ms (63.48%), Space: 19.3 MB (82.87%) - LeetHub

---
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def scoreDifference(self, nums: List[int]) -> int:
+        a=0
+        b=0
+        chance=True
+        for i in range(0,len(nums)):
+            if (i+1)%6==0:
+                chance=not chance
+
+            if nums[i]%2==1:
+                chance=not chance
+
+
+            if chance:
+                a+=nums[i]    
+            else:
+                b+=nums[i]
+
+        return a-b        
